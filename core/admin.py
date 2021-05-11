bc84ce9bff9e5c928221db2c6d066faa4a11cc18
Josue Silva <josuesilvarosa20@hotmail.com>
2021-05-11 16:03:21 -0300

    Criado tabela do dentas de eventos para trabalha com a agenda

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from core.models import Dentista
 
 # Register your models here.
+
+class EventoAdmin(admin.ModelAdmin):
+     list_display = ('titulo',  'data_evento', 'data_criacao')
+     list_filter = ('usuario', 'data_evento', 'data_criacao')
+
+admin.site.register(Dentista, EventoAdmin)
